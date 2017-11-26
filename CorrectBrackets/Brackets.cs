0cc2a386561809b78926f0a0505ffc5983c9232a
orlindraganov <orlindraganov@gmail.com>
2017-11-26 18:53:53 +0200

    Working 2.Brackets

---
+++ CorrectBrackets/Brackets.cs
@@ -0,0 +1,40 @@
+﻿namespace CorrectBrackets
+{
+    using System;
+
+    class Brackets
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            int bracketCounter = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentSymbol = input[i];
+
+                if (currentSymbol == '(')
+                {
+                    bracketCounter++;
+                }
+                else if (currentSymbol == ')')
+                {
+                    bracketCounter--;
+                }
+            }
+
+            string output;
+
+            if (bracketCounter == 0)
+            {
+                output = "Correct";
+            }
+            else
+            {
+                output = "Incorrect";
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
