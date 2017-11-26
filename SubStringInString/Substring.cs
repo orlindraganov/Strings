5d05d16a289fe9720df019ecced86352f2dafe52
orlindraganov <orlindraganov@gmail.com>
2017-11-26 19:25:38 +0200

    Done 4.Substring Corrected task numbers

---
+++ SubStringInString/Substring.cs
@@ -0,0 +1,25 @@
+﻿namespace SubStringInString
+{
+    using System;
+
+    class Substring
+    {
+        static void Main()
+        {
+            string pattern = Console.ReadLine().ToLower();
+            string text = Console.ReadLine().ToLower();
+
+            int counter = 0;
+
+            int index = text.IndexOf(pattern);
+
+            while (0 <= index && index < text.Length)
+            {
+                counter++;
+                index = text.IndexOf(pattern, index + 1);
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
