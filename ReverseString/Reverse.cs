b41f9022066ac41b3e7435113f5de5e9fbd71ff2
orlindraganov <orlindraganov@gmail.com>
2017-11-26 18:42:58 +0200

    Working 1.ReverseString

---
+++ ReverseString/Reverse.cs
@@ -0,0 +1,27 @@
+﻿namespace ReverseString
+{
+    using System;
+
+    class Reverse
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+
+
+
+            char[] processedChars = new char[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var inputIndex = input.Length - i - 1;
+
+                processedChars[i] = input[inputIndex];
+            }
+
+            var output = new string(processedChars);
+
+            Console.WriteLine(output);
+        }
+    }
+}
