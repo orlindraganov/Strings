f603885b8659a299a1c32105dc9d9f347032031c
orlindraganov <orlindraganov@gmail.com>
2017-11-28 15:14:48 +0200

    Solved 08.Extract Sentences and 10.Unicode Characters

---
+++ ExtractSentences/Extract.cs
@@ -0,0 +1,34 @@
+﻿namespace ExtractSentences
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    class Extract
+    {
+        static void Main()
+        {
+            var pattern = Console.ReadLine();
+            var input = Console.ReadLine();
+
+            char[] sentenceSeparators = { '.' };
+            char[] wordSeparators = input.Where(c => !char.IsLetter(c)).Distinct().ToArray();
+
+            var builder = new StringBuilder();
+            var sentences = input.Split(sentenceSeparators, StringSplitOptions.RemoveEmptyEntries).Select(s => s.Trim()).ToArray();
+
+            foreach (var sentence in sentences)
+            {
+                var words = sentence.Split(wordSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+                if (words.Contains(pattern))
+                {
+                    builder.Append(sentence);
+                    builder.Append(". ");
+                }
+            }
+
+            Console.WriteLine(builder.ToString().Trim());
+        }
+    }
+}
---
+++ UnicodeCharacters/Unicode.cs
@@ -0,0 +1,27 @@
+﻿namespace UnicodeCharacters
+{
+    using System;
+    using System.Text;
+
+    class Unicode
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var builder = new StringBuilder();
+
+            foreach (var symbol in input)
+            {
+                builder.Append(ConvertToUnicode(symbol));
+            }
+
+            Console.WriteLine(builder);
+        }
+
+        private static string ConvertToUnicode(char input)
+        {
+            return @"\u" + ((int)input).ToString("X4");
+        }
+    }
+}
